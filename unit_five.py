90c311f0a2b862b1b12de0bd73db8ab3a1eb9569
Solbjor <s0lbjorandr3w@gmail.com>
2025-07-05 12:03:08 -0700

    Update unit_five.py

--- unit_five.py
+++ unit_five.py
@@ -13,3 +13,25 @@ print(apollo.name)
 print(apollo.species)  
 print(apollo.catchphrase) 
 print(apollo.furniture) 
+
+######################################################
+
+# Test behavior to make sure its what you want, in Test Driven Development (or Behavior Driven Development)
+
+######################################################
+
+class Villager:
+    def __init__(self, name, species, catchphrase):
+        self.name = name
+        self.species = species
+        self.catchphrase = catchphrase
+        self.furniture = []
+        
+    def greet_player(self, player_name):
+        return f"{self.name}: Hey there, {player_name}! How's it going, {self.catchphrase}!"
+
+name = Villager('Bones', 'Human', 'bonehead')
+print(name.greet_player('Andrew'))
+
+######################################################
+
