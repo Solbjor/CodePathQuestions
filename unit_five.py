eafdcece2f168c3d9646ccc756bbfd944a72da44
Solbjor <s0lbjorandr3w@gmail.com>
2025-07-05 10:34:12 -0700

    Update unit_five.py

--- unit_five.py
+++ unit_five.py
@@ -1,2 +1,15 @@
-#lol#
-#new commit
+class Villager:
+    def __init__(self, name, species, catchphrase):
+        self.name = name
+        self.species = species
+        self.catchphrase = catchphrase
+        self.furniture = []
+
+
+# Instantiate your villager here
+apollo = Villager('Marcus', 'Human', 'Dang Bugs!')
+
+print(apollo.name)  
+print(apollo.species)  
+print(apollo.catchphrase) 
+print(apollo.furniture) 
