bae240a7ddd6c22b43dec8a2aca8617dbf381b9c
Solbjor <s0lbjorandr3w@gmail.com>
2025-07-05 17:39:50 -0700

    LinkedList pt2

--- unit_five.py
+++ unit_five.py
@@ -127,12 +127,27 @@ class Node:
 tom_nook = Node("Tom Nook")
 tommy = Node("Tommy") 
 tom_nook.next = tommy 
+
+#Make timmy next node of Tom Nook
 timmy = Node("Timmy")
 tom_nook.next = timmy
 timmy.next = tommy
-print(tom_nook.value)
-print(tom_nook.next.value)
-print(timmy.value)
-print(timmy.next.value)
-print(tommy.value)
-print(tommy.next)
+
+#print(tom_nook.value)
+#print(tom_nook.next.value)
+#print(timmy.value)
+#print(timmy.next.value)
+#print(tommy.value)
+#print(tommy.next)
+
+saharah = Node("Saharah")
+tom_nook.next = None
+tommy.next = saharah
+print(tom_nook.next) 
+print(timmy.value) 
+print(timmy.next.value)  
+print(tommy.value) 
+print(tommy.next.value)
+print(saharah.value)  
+print(saharah.next) 
+print(tom_nook)
