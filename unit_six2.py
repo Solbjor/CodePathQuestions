0ac8702fdf7003f4535549f3d6a720a9ea521e28
Solbjor <s0lbjorandr3w@gmail.com>
2025-07-13 01:07:44 -0700

    Update unit_six2.py

--- unit_six2.py
+++ unit_six2.py
@@ -1,3 +1,4 @@
+''''
 class Node:
     def __init__(self, value, next=None):
         self.value = value
@@ -21,3 +22,31 @@ clue2.next = clue3
 clue3.next = clue1
 
 print(is_circular(clue1))
+'''
+
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+def collect_false_evidence(evidence):
+    false_evidence = []
+    
+
+clue1 = Node("Unmarked sedan seen near the crime scene")
+clue2 = Node("The stolen goods are at an abandoned warehouse")
+clue3 = Node("The mayor is accepting bribes")
+clue4 = Node("They dumped their disguise in the lake")
+clue1.next = clue2
+clue2.next = clue3
+clue3.next = clue4
+clue4.next = clue2
+
+clue5 = Node("A masked figure was seen fleeing the scene")
+clue6 = Node("Footprints lead to the nearby woods")
+clue7 = Node("A broken window was found at the back")
+clue5.next = clue6
+clue6.next = clue7
+
+print(collect_false_evidence(clue1))
+print(collect_false_evidence(clue5))
