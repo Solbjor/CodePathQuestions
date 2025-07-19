3e3a27f07a3fc52ee87c99bddd7e70d70c370239
Solbjor <s0lbjorandr3w@gmail.com>
2025-07-19 11:52:17 -0700

    recursion

--- unit_seven.py
+++ unit_seven.py
@@ -12,5 +12,32 @@ def fibonacci(num, memo={}):
     
     return memo[num]
 
-
 print(fibonacci(600))
+
+
+def count_suits_iterative(suits):
+    count = 0
+    for suit in suits: 
+        count+=1 
+    return count
+def count_suits_recursive(suits ):
+    # base case 
+    count = 0 
+    if len(suits) == count:
+        return count
+    count += 1
+    return 1 + count_suits_recursive(suits)
+
+print(count_suits_iterative(["Mark I", "Mark II", "Mark III"]))
+print(count_suits_recursive(["Mark I", "Mark I", "Mark III", "Mark IV"]))
+
+
+
+def sum_stones(stones):
+    
+    if not stones:
+        return 0
+    return stones[0] + sum_stones(stones[1:])
+
+print(sum_stones([5, 10, 15, 20, 25, 30]))
+print(sum_stones([12, 8, 22, 16, 10]))
