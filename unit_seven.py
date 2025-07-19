cbfb96b2d42ebd4ed8bb614f3d510d592877221f
Solbjor <s0lbjorandr3w@gmail.com>
2025-07-19 10:45:26 -0700

    Fibonacci with Memo

---
+++ unit_seven.py
@@ -0,0 +1,16 @@
+# Fibonnacci with Memo, memo servers as a cache and stores the numbers we've already encountered
+
+def fibonacci(num, memo={}):
+    if num in memo:
+        return memo[num]
+    if num == 0:
+        return 0
+    elif num == 1:
+        return 1
+    
+    memo[num] = fibonacci(num - 1, memo) + fibonacci(num - 2, memo)
+    
+    return memo[num]
+
+
+print(fibonacci(600))
